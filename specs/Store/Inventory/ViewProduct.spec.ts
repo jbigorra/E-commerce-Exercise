86ac1cca769e5444d14210b9ddd0f5275eb0e875
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-06-05 11:12:55 +0200

    Refactors ViewProduct spec for clarity
    Improves the structure of the ViewProduct spec file by using `beforeEach` to reduce code duplication and group tests into Errors and Success categories. This enhances readability and maintainability of the test suite.

--- specs/Store/Inventory/ViewProduct.spec.ts
+++ specs/Store/Inventory/ViewProduct.spec.ts
@@ -7,6 +7,7 @@ import {
   InMemoryInventory,
   ProductRepository,
 } from "../../../src/Store/Inventory/Infrastructure/InMemoryInventory";
+import { IInventory } from "../../../src/Store/Inventory/Interfaces";
 import {
   CUSTOMIZABLE_PRODUCT_ID,
   NOT_FOUND_PRODUCT_ID,
@@ -16,75 +17,80 @@ import {
 import { expectError, expectSuccess } from "../../Helpers/forActions/Matchers";
 
 describe("ViewProduct", () => {
-  it("should return the standard product with empty available options", () => {
-    const products: Product[] = productsFixture();
-    const inventory = new InMemoryInventory(new ProductRepository(products));
-    const action = new ViewProduct(inventory);
+  let products: Product[];
+  let inventory: IInventory;
 
-    const actionResult = action.execute(
-      new ViewProductCommand(STANDARD_PRODUCT_ID)
-    );
-
-    expectSuccess<Product>(actionResult, {
-      id: STANDARD_PRODUCT_ID,
-      type: "standard",
-      availableOptions: (opts: Product["availableOptions"]) =>
-        expect(opts).toEqual([]),
-    });
+  beforeEach(() => {
+    products = productsFixture();
+    inventory = new InMemoryInventory(new ProductRepository(products));
   });
 
-  it("should return the customizable product with available options", () => {
-    const products: Product[] = productsFixture();
-    const inventory = new InMemoryInventory(new ProductRepository(products));
-    const action = new ViewProduct(inventory);
-
-    const actionResult = action.execute(
-      new ViewProductCommand(CUSTOMIZABLE_PRODUCT_ID)
-    );
-
-    expectSuccess<Product>(actionResult, {
-      id: CUSTOMIZABLE_PRODUCT_ID,
-      type: "customizable",
-      availableOptions: (opts: Product["availableOptions"]) =>
-        expect(opts.length).toBeGreaterThan(0),
-    });
-  });
-
-  it("should return an error if the product is not found", () => {
-    const products: Product[] = productsFixture();
-    const inventory = new InMemoryInventory(new ProductRepository(products));
-    const action = new ViewProduct(inventory);
-    const actionResult = action.execute(
-      new ViewProductCommand(NOT_FOUND_PRODUCT_ID)
-    );
-    expectError(actionResult, "Product not found");
-  });
-
-  it.each([
-    {
-      id: STANDARD_PRODUCT_ID,
-      expectedType: "standard" as const,
-      expectedBasePrice: 20,
-    },
-    {
-      id: CUSTOMIZABLE_PRODUCT_ID,
-      expectedType: "customizable" as const,
-      expectedBasePrice: 20,
-    },
-  ])(
-    "should return the product with the base price",
-    ({ id, expectedType, expectedBasePrice }) => {
-      const products: Product[] = productsFixture();
-      const inventory = new InMemoryInventory(new ProductRepository(products));
+  describe("Errors", () => {
+    it("should return an error if the product is not found", () => {
       const action = new ViewProduct(inventory);
 
-      const actionResult = action.execute(new ViewProductCommand(id));
+      const actionResult = action.execute(
+        new ViewProductCommand(NOT_FOUND_PRODUCT_ID)
+      );
+      expectError(actionResult, "Product not found");
+    });
+  });
+
+  describe("Success", () => {
+    it("should return the standard product with empty available options", () => {
+      const action = new ViewProduct(inventory);
+
+      const actionResult = action.execute(
+        new ViewProductCommand(STANDARD_PRODUCT_ID)
+      );
 
       expectSuccess<Product>(actionResult, {
-        id,
-        type: expectedType,
-        basePrice: expectedBasePrice,
+        id: STANDARD_PRODUCT_ID,
+        type: "standard",
+        availableOptions: (opts: Product["availableOptions"]) =>
+          expect(opts).toEqual([]),
       });
-    }
-  );
+    });
+
+    it("should return the customizable product with available options", () => {
+      const action = new ViewProduct(inventory);
+
+      const actionResult = action.execute(
+        new ViewProductCommand(CUSTOMIZABLE_PRODUCT_ID)
+      );
+
+      expectSuccess<Product>(actionResult, {
+        id: CUSTOMIZABLE_PRODUCT_ID,
+        type: "customizable",
+        availableOptions: (opts: Product["availableOptions"]) =>
+          expect(opts.length).toBeGreaterThan(0),
+      });
+    });
+
+    it.each([
+      {
+        id: STANDARD_PRODUCT_ID,
+        expectedType: "standard" as const,
+        expectedBasePrice: 20,
+      },
+      {
+        id: CUSTOMIZABLE_PRODUCT_ID,
+        expectedType: "customizable" as const,
+        expectedBasePrice: 20,
+      },
+    ])(
+      "should return the product with the base price",
+      ({ id, expectedType, expectedBasePrice }) => {
+        const action = new ViewProduct(inventory);
+
+        const actionResult = action.execute(new ViewProductCommand(id));
+
+        expectSuccess<Product>(actionResult, {
+          id,
+          type: expectedType,
+          basePrice: expectedBasePrice,
+        });
+      }
+    );
+  });
 });
