f44d2ed99726c0c6f9b3e80909cdbd0958bbcc42
Juan bigorra <10943260+jbigorra@users.noreply.github.com>
2025-06-13 17:44:56 +0200

    Adds Jest test script to package.json and removes constraint engine dependency from ServiceFactory. Simplifies product customization service instantiation by eliminating unnecessary parameters.

--- src/Store/ProductCatalog/Core/Services/ServiceFactory.ts
+++ src/Store/ProductCatalog/Core/Services/ServiceFactory.ts
@@ -1,21 +1,14 @@
-import { ConstraintEngine } from "../Constraints/ConstraintEngine";
 import { ChoiceSelectionService } from "./ChoiceSelectionService";
 import { DefaultProductCustomizationService, ProductCustomizationService } from "./ProductCustomizationService";
 
 export class ServiceFactory {
   private static choiceSelectionService: ChoiceSelectionService | null = null;
-  private static constraintEngine: ConstraintEngine | null = null;
   private static productCustomizationService: ProductCustomizationService | null = null;
 
   static createProductCustomizationService(): ProductCustomizationService {
     if (!this.productCustomizationService) {
       const choiceSelectionService = this.createChoiceSelectionService();
-      const constraintEngine = this.createConstraintEngine();
-
-      this.productCustomizationService = new DefaultProductCustomizationService(
-        choiceSelectionService,
-        constraintEngine
-      );
+      this.productCustomizationService = new DefaultProductCustomizationService(choiceSelectionService);
     }
 
     return this.productCustomizationService;
@@ -28,17 +21,9 @@ export class ServiceFactory {
     return this.choiceSelectionService;
   }
 
-  static createConstraintEngine(): ConstraintEngine {
-    if (!this.constraintEngine) {
-      this.constraintEngine = new ConstraintEngine();
-    }
-    return this.constraintEngine;
-  }
-
   // For testing purposes
   static reset(): void {
     this.choiceSelectionService = null;
-    this.constraintEngine = null;
     this.productCustomizationService = null;
   }
 }
